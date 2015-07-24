bd0093aa1f7ace3b178b7bdc5ddb6e3451574a2a
Alexander Miranda <alexandermichaelmiranda@gmail.com>
2015-07-24 15:58:24 -0400

    Updated scraper to handle when some data nodes are undefined for certain gardens

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import requests
 import bs4
+import re
+from time import sleep
 
 root_url = 'http://www.seattle.gov/'
 index_url = root_url + 'neighborhoods/p-patch-community-gardening/p-patch-list'
@@ -16,11 +18,27 @@ def get_garden_data(garden_url):
    soup = bs4.BeautifulSoup(response.text, 'html.parser')
    garden_data['name'] = soup.select('h1.pageTitle span')[0].get_text()
    garden_data['features'] = [a.get_text() for a in soup.select('ul.features li')]
+   garden_data['address'] = re.sub('Address: ', '', soup.select('div.Address')[0].get_text())
+   if soup.has_attr('div.Numberofplots'):
+      garden_data['num_of_plots'] = re.sub('Number Of Plots: ', '', soup.select('div.Numberofplots')[0].get_text())
+   garden_data['estbl'] = re.sub('Established: ', '', soup.select('div.Established')[0].get_text())
+   if soup.has_attr('div.size'):
+      garden_data['size'] = re.sub('Size: ', '', soup.select('div.size')[0].get_text())
+   if soup.has_attr('div.waitTime'):
+      garden_data['wait_time'] = re.sub('Wait Time: ', '', soup.select('div.waitTime')[0].get_text())
+   garden_data['description'] = soup.select('div.span')[0].get_text()
    return garden_data
 
 def show_garden_stats():
    garden_urls = get_garden_urls()
+   i = 0
+   progress_str = '.'
    for garden_url in garden_urls:
-      print get_garden_data(garden_url)
+      sleep(1)
+      i += 1
+      if i % 5 == 0:
+         progress_str += '.'
+         print progress_str
+   print 'scrape complete with no errors'
 
 show_garden_stats()
