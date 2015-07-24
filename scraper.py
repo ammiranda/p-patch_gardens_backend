f9ac3e4eec5c66f8f3239f8b704ffef9a063bbcc
Alexander Miranda <alexandermichaelmiranda@gmail.com>
2015-07-24 16:48:49 -0400

    Updated the scraper to write the garden data into a json file

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import requests
 import bs4
 import re
 from time import sleep
+import json
 
 root_url = 'http://www.seattle.gov/'
 index_url = root_url + 'neighborhoods/p-patch-community-gardening/p-patch-list'
@@ -21,7 +22,8 @@ def get_garden_data(garden_url):
    garden_data['address'] = re.sub('Address: ', '', soup.select('div.Address')[0].get_text())
    if soup.has_attr('div.Numberofplots'):
       garden_data['num_of_plots'] = re.sub('Number Of Plots: ', '', soup.select('div.Numberofplots')[0].get_text())
-   garden_data['estbl'] = re.sub('Established: ', '', soup.select('div.Established')[0].get_text())
+   if soup.has_attr('div.Established'):
+      garden_data['estbl'] = re.sub('Established: ', '', soup.select('div.Established')[0].get_text())
    if soup.has_attr('div.size'):
       garden_data['size'] = re.sub('Size: ', '', soup.select('div.size')[0].get_text())
    if soup.has_attr('div.waitTime'):
@@ -30,15 +32,15 @@ def get_garden_data(garden_url):
    return garden_data
 
 def show_garden_stats():
+   data = {}
    garden_urls = get_garden_urls()
-   i = 0
-   progress_str = '.'
    for garden_url in garden_urls:
       sleep(1)
-      i += 1
-      if i % 5 == 0:
-         progress_str += '.'
-         print progress_str
-   print 'scrape complete with no errors'
+      garden_data = get_garden_data(garden_url)
+      data[garden_data['name']] = garden_data
+   out_file = open("gardens.txt", "w") 
+   json_data = json.dumps(data, indent=3)
+   out_file.write(json_data)
+   out_file.close()
 
 show_garden_stats()
