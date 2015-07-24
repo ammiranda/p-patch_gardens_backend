70f43d43de374fc22300196ccc073db690bf154f
Alexander Miranda <alexandermichaelmiranda@gmail.com>
2015-07-24 17:33:46 -0400

    Trying to remove special chars from the scraped data

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup as bs
 import re
 from time import sleep
 import json
@@ -9,14 +9,14 @@ index_url = root_url + 'neighborhoods/p-patch-community-gardening/p-patch-list'
 
 def get_garden_urls():
    response = requests.get(index_url)
-   soup = bs4.BeautifulSoup(response.text, 'html.parser')
+   soup = bs(response.text, 'html.parser')
    return [a.attrs.get('href') for a in soup.select('div.paginationContainer a')]
 
 def get_garden_data(garden_url):
    garden_data = {}
    print root_url + garden_url
    response = requests.get(root_url + garden_url)
-   soup = bs4.BeautifulSoup(response.text, 'html.parser')
+   soup = bs(response.text, 'html.parser')
    garden_data['name'] = soup.select('h1.pageTitle span')[0].get_text()
    garden_data['features'] = [a.get_text() for a in soup.select('ul.features li')]
    garden_data['address'] = re.sub('Address: ', '', soup.select('div.Address')[0].get_text())
